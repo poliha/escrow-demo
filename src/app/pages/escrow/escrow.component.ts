98d61c0b718cdcb210467313e05bd7120875edfb
oliha <poliha2002@yahoo.com>
2018-06-13 14:31:52 +0100

    create accounts

--- src/app/pages/escrow/escrow.component.ts
+++ src/app/pages/escrow/escrow.component.ts
@@ -32,5 +32,52 @@ export class EscrowComponent implements OnInit {
     return { publicKey: pair.publicKey(), privateKey: pair.secret() };
   }
 
+  startDemo() {
+    console.log('1. Generate Keypairs ...');
+    this.srcAccount = this.generateAccount();
+    this.destAccount = this.generateAccount();
+    this.escrowAccount = this.generateAccount();
+    console.log('2. Keypairs Generated');
 
+  }
+
+  createAccounts() {
+    console.log('3. Creating accounts ... ');
+    StellarSdk.Network.useTestNetwork();
+    const server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+    const sourceKeys = StellarSdk.Keypair.fromSecret(this.originAccount.privateKey);
+
+    server.loadAccount(this.originAccount.publicKey)
+      .catch((error) => {
+        console.error(error);
+        throw new Error('The origin account does not exist!');
+      })
+      .then((source) => {
+        // Start building the transaction.
+        let transaction = new StellarSdk.TransactionBuilder(source)
+          .addOperation(StellarSdk.Operation.createAccount({
+            destination: this.srcAccount.publicKey,
+            startingBalance: this.startingBalance
+          }))
+          .addOperation(StellarSdk.Operation.createAccount({
+            destination: this.destAccount.publicKey,
+            startingBalance: this.startingBalance
+          }))
+          .addOperation(StellarSdk.Operation.createAccount({
+            destination: this.escrowAccount.publicKey,
+            startingBalance: this.startingBalance
+          }))
+          .addMemo(StellarSdk.Memo.text('Test Transaction'))
+          .build();
+        transaction.sign(sourceKeys);
+        return server.submitTransaction(transaction);
+      })
+      .then(function (result) {
+        console.log('Success! Results:', result);
+        console.log('4. Accounts created ');
+      })
+      .catch(function (error) {
+        console.error('Something went wrong!', error);
+      });
+  }
 }
